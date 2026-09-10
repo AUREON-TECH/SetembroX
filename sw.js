e9fc447c32b080e36117ffd84b91b2c5d4e6efcb
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-09-09 22:56:37 -0300

    feat: improve offline cache strategy v2

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE='esteticagyr-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
+const CACHE='esteticagyr-v2.1';
+const CORE=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(res=>{
+    const copy=res.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
+    return res;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
+self.addEventListener('message',e=>{if(e.data==='SKIP_WAITING') self.skipWaiting()});
