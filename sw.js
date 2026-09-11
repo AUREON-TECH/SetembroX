0e33466a7875721fdc0d137cc8b0656e3ae60ded
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-09-10 23:06:36 -0300

    chore: refresh Setembro X PWA cache for mission control

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='setembrox-v1.0.0';
+const CACHE='setembrox-v2.0.0';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
