99a824474af7b1de76b94c63fef5e9bd002cdfce
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-09-22 09:00:35 -0300

    pwa: renovar cache para dados de 21-09

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Dados oficiais atualizados até 20/09/2026; alteração deste arquivo força atualização do shell.
-const CACHE = 'setembrox-v17-data-2009';
+// Dados oficiais atualizados até 21/09/2026; alteração deste arquivo força atualização do shell.
+const CACHE = 'setembrox-v18-data-2109';
 const CORE = ['./index.html', './styles.css', './entry-v2.css', './arena-x.css', './app.js', './sales-count-fix.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 const SENSITIVE_PARAMS = /(^|_)(token|access_token|refresh_token|password|senha|secret|session|auth|authorization|code|credential|credentials|api_key|apikey)(_|$)/i;
 const PRIVATE_PATHS = /\/(api|auth|login|logout|session|sessions|account|profile|admin)(\/|$)/i;
