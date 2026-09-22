3b297c846c205e2996ca4798a94225401b64153f
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-09-22 09:07:34 -0300

    pwa: incluir central operacional no cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Dados oficiais atualizados até 21/09/2026; alteração deste arquivo força atualização do shell.
-const CACHE = 'setembrox-v18-data-2109';
-const CORE = ['./index.html', './styles.css', './entry-v2.css', './arena-x.css', './app.js', './sales-count-fix.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
+const CACHE = 'setembrox-v19-ops-dashboard';
+const CORE = ['./index.html', './styles.css', './entry-v2.css', './arena-x.css', './ops-dashboard.css', './app.js', './sales-count-fix.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 const SENSITIVE_PARAMS = /(^|_)(token|access_token|refresh_token|password|senha|secret|session|auth|authorization|code|credential|credentials|api_key|apikey)(_|$)/i;
 const PRIVATE_PATHS = /\/(api|auth|login|logout|session|sessions|account|profile|admin)(\/|$)/i;
 
